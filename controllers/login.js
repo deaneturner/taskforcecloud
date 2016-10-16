2f88f805605882130897af78788e3dd1a1f88f16
deaneturner <service@deaneturner.com>
2016-10-16 18:01:48 -0400

    login

--- controllers/login.js
+++ controllers/login.js
@@ -35,13 +35,37 @@ module.exports = function (lib) {
         // }
 
 
+        // var userModel = lib.db.model('User');
+        // userModel.find().exec(function (err, user) {
+        //     if (err) return next(controller.RESTError('InternalServerError', err));
+        //
+        //     controller.writeHAL(res, {
+        //         id_token: createToken(userModel({username: 'test', password: 'XXX'}))
+        //     });
+        // });
+
         var userModel = lib.db.model('User');
-        userModel.find().exec(function (err, user) {
+        userModel.findOne({
+            username: req.params.username
+        }).exec(function (err, user) {
             if (err) return next(controller.RESTError('InternalServerError', err));
 
-            controller.writeHAL(res, {
-                id_token: createToken(userModel({username: 'test', password: 'XXX'}))
-            });
+            if (!user) {
+                res.status(403).send({success: false, msg: 'Authentication failed, User not found'});
+            }
+
+            else {
+                user.comparePassword(req.params.password, function (err, isMatch) {
+                    if (isMatch && !err) {
+                        controller.writeHAL(res, {
+                            id_token: createToken(userModel({username: 'test', password: 'XXX'}))
+                        });
+                    } else {
+                        return res.status(403).send({success: false, msg: 'Authenticaton failed, wrong password.'});
+                    }
+                })
+            }
+
         });
     });
 
