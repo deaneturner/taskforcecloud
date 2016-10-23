f7c2e4f934c23caaf786dd7c8c37a6ee6d2d7014
deaneturner <service@deaneturner.com>
2016-10-23 17:14:11 -0400

    users

--- public/app/dashboard/dashboard.module.ts
+++ public/app/dashboard/dashboard.module.ts
@@ -2,10 +2,12 @@ import 'jquery-ui/ui/sortable.js';
 
 import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
-
 import { RouterModule } from '@angular/router';
+
 import { Dashboard } from './dashboard.component';
 import { GridComponent } from '../common/list/grid.component';
+import { GridDemo } from '../common/list/grid-demo/grid-demo';
+
 import { AppAuth } from '../app.auth';
 
 export const routes = [
@@ -15,7 +17,7 @@ export const routes = [
 
 @NgModule({
   imports: [ CommonModule, RouterModule.forChild(routes) ],
-  declarations: [ Dashboard, GridComponent ]
+  declarations: [ Dashboard, GridComponent, GridDemo ]
 })
 export default class DashboardModule {
   static routes = routes;
