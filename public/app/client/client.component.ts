71920264411adb7cb5f8f4e7e7de93708e7f8374
deaneturner <service@deaneturner.com>
2016-10-31 13:57:45 -0400

    list config

--- public/app/client/client.component.ts
+++ public/app/client/client.component.ts
@@ -1,41 +1,41 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { AppConfig } from '../app.config';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {AppConfig} from '../app.config';
 declare var jQuery: any;
 
 @Component({
-  selector: 'client',
-  templateUrl: './client.template.html',
-  styleUrls: ['./client.style.scss'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'client',
+    templateUrl: './client.template.html',
+    styleUrls: ['./client.style.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class Client {
-  config: any;
-  month: any;
-  year: any;
-  sortOptions: Object = {
-    connectWith: '.widget-container',
-    handle: 'header, .handle',
-    cursor: 'move',
-    iframeFix: false,
-    items: '.widget:not(.locked)',
-    opacity: 0.8,
-    helper: 'original',
-    revert: true,
-    forceHelperSize: true,
-    placeholder: 'widget widget-placeholder',
-    forcePlaceholderSize: true,
-    tolerance: 'pointer'
-  };
+    config: any;
+    month: any;
+    year: any;
+    sortOptions: Object = {
+        connectWith: '.widget-container',
+        handle: 'header, .handle',
+        cursor: 'move',
+        iframeFix: false,
+        items: '.widget:not(.locked)',
+        opacity: 0.8,
+        helper: 'original',
+        revert: true,
+        forceHelperSize: true,
+        placeholder: 'widget widget-placeholder',
+        forcePlaceholderSize: true,
+        tolerance: 'pointer'
+    };
 
-  constructor(config: AppConfig) {
-    this.config = config.getConfig();
-  }
+    constructor(config: AppConfig) {
+        this.config = config.getConfig();
+    }
 
-  ngOnInit(): void {
-    jQuery('.widget-container').sortable(this.sortOptions);
+    ngOnInit(): void {
+        jQuery('.widget-container').sortable(this.sortOptions);
 
-    let now = new Date();
-    this.month = now.getMonth() + 1;
-    this.year = now.getFullYear();
-  }
+        let now = new Date();
+        this.month = now.getMonth() + 1;
+        this.year = now.getFullYear();
+    }
 }
--- public/app/shared/list/list.component.ts
+++ public/app/shared/list/list.component.ts
@@ -1,15 +1,16 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {Component, ViewEncapsulation, Input} from '@angular/core';
 declare var jQuery: any;
 
 @Component({
-  selector: '[list]',
-  templateUrl: './list.template.html',
-  encapsulation: ViewEncapsulation.None,
-  styleUrls: ['./list.style.scss']
+    selector: '[list]',
+    templateUrl: './list.template.html',
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: ['./list.style.scss']
 })
 export class ListComponent {
+    @Input() listConfig: any;
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-  }
+    }
 }
