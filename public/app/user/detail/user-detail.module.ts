b4f5ee9c86978dc5d8249cc8ec6a11ee27e9450b
deaneturner <service@deaneturner.com>
2016-11-14 16:10:33 -0500

    user profile

---
+++ public/app/user/detail/user-detail.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { UserDetailComponent } from './user-detail.component';
+
+export const routes = [
+  {path: '', component: UserDetailComponent, pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [
+    // Components / Directives/ Pipes
+    UserDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export default class UserDetailModule {
+  static routes = routes;
+}
