ba7874e1e5d3911c55be07c7852b1b19be963fec
deaneturner <service@deaneturner.com>
2016-11-20 15:17:53 -0500

    user to users

--- public/app/users/users.module.ts
+++ public/app/users/users.module.ts
@@ -3,12 +3,13 @@ import {CommonModule}  from '@angular/common';
 
 import {UserDetailComponent} from './detail/user-detail.component';
 import {UserEditComponent} from './edit/user-edit.component';
+import {UserListModule} from '../users/list/user-list.module';
 import {PanelModule} from '../shared/panel/panel.module';
 
 import {ROUTES}       from './users.routes';
 
 @NgModule({
-    imports: [CommonModule, PanelModule, ROUTES],
+    imports: [CommonModule, UserListModule, PanelModule, ROUTES],
     declarations: [UserDetailComponent, UserEditComponent]
 })
 
