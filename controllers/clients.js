460cb5a097efe1fb84deeb4821ee14811fbe85d6
deaneturner <service@deaneturner.com>
2016-12-27 00:16:00 -0500

    fix get client

--- controllers/clients.js
+++ controllers/clients.js
@@ -11,6 +11,26 @@ module.exports = function(lib) {
     var controller = new Clients();
 
     controller.addAction({
+        'path': '/api/clients/{id}',
+        'method': 'GET',
+        'params': [swagger.pathParam('id', 'The id of the client', 'string')],
+        'summary': 'Returns the data of one client',
+        'responsClass': 'Client',
+        'nickname': 'getClient'
+    }, function(req, res, next) {
+        var id = req.params.id;
+        if (id != null) {
+            lib.db.model('Client').findOne({_id: id}).exec(function(err, client) {
+                if (err) return next(controller.RESTError('InternalServerError', err));
+                if (!client) return next(controller.RESTError('ResourceNotFoundError', 'The client id cannot be found'));
+                controller.writeHAL(res, client);
+            });
+        } else {
+            next(controller.RESTError('InvalidArgumentError', 'Invalid client id'));
+        }
+    });
+
+    controller.addAction({
         'path': '/api/clients',
         'method': 'GET',
         'summary': 'Returns the list of clients ordered by name',
@@ -37,26 +57,6 @@ module.exports = function(lib) {
             if (err) return next(controller.RESTError('InternalServerError', err));
             controller.writeHAL(res, {success: true, data: client});
         });
-    });
-
-    controller.addAction({
-        'path': '/api/clients/{id}',
-        'method': 'GET',
-        'params': [swagger.pathParam('id', 'The id of the client', 'string')],
-        'summary': 'Returns the data of one client',
-        'responsClass': 'Client',
-        'nickname': 'getClient'
-    }, function(req, res, next) {
-        var id = req.params.id;
-        if (id != null) {
-            lib.db.model('Client').findOne({_id: id}).exec(function(err, client) {
-                if (err) return next(controller.RESTError('InternalServerError', err));
-                if (!client) return next(controller.RESTError('ResourceNotFoundError', 'The client id cannot be found'));
-                controller.writeHAL(res, client);
-            });
-        } else {
-            next(controller.RESTError('InvalidArgumentError', 'Invalid client id'));
-        }
     });
 
     controller.addAction({
