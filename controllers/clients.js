1cd477d94e0b2362a737e8db1642a75d0389564b
deaneturner <service@deaneturner.com>
2016-12-26 23:56:44 -0500

    fix get client

--- controllers/clients.js
+++ controllers/clients.js
@@ -39,25 +39,25 @@ module.exports = function(lib) {
         });
     });
 
-    controller.addAction({
-        'path': '/api/clients/{id}',
-        'method': 'GET',
-        'params': [swagger.pathParam('id', 'The id of the client', 'string')],
-        'summary': 'Returns the data of one client',
-        'responsClass': 'Client',
-        'nickname': 'getClient'
-    }, function(req, res, next) {
-        var id = req.params.id;
-        if (id != null) {
-            lib.db.model('Client').findOne({_id: id}).exec(function(err, client) {
-                if (err) return next(controller.RESTError('InternalServerError', err));
-                if (!client) return next(controller.RESTError('ResourceNotFoundError', 'The client id cannot be found'));
-                controller.writeHAL(res, client);
-            });
-        } else {
-            next(controller.RESTError('InvalidArgumentError', 'Invalid client id'));
-        }
-    });
+    // controller.addAction({
+    //     'path': '/api/clients/{id}',
+    //     'method': 'GET',
+    //     'params': [swagger.pathParam('id', 'The id of the client', 'string')],
+    //     'summary': 'Returns the data of one client',
+    //     'responsClass': 'Client',
+    //     'nickname': 'getClient'
+    // }, function(req, res, next) {
+    //     var id = req.params.id;
+    //     if (id != null) {
+    //         lib.db.model('Client').findOne({_id: id}).exec(function(err, client) {
+    //             if (err) return next(controller.RESTError('InternalServerError', err));
+    //             if (!client) return next(controller.RESTError('ResourceNotFoundError', 'The client id cannot be found'));
+    //             controller.writeHAL(res, client);
+    //         });
+    //     } else {
+    //         next(controller.RESTError('InvalidArgumentError', 'Invalid client id'));
+    //     }
+    // });
 
     controller.addAction({
         'path': '/api/clients/{id}',
