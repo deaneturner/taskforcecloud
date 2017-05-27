7670ba1f64685938668ba1cb7ed03470aefb953d
deaneturner <service@deaneturner.com>
2017-05-27 17:41:38 -0400

    dist

--- public/app/environment.ts
+++ public/app/environment.ts
@@ -14,7 +14,7 @@ let _decorateModuleRef = function identity<T>(value: T): T { return value; };
 
 if ('production' === ENV) {
   // Production
-  // disableDebugTools();
+  disableDebugTools();
   enableProdMode();
 
   PROVIDERS = [
